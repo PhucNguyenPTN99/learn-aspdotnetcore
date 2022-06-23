a6b1b7ebe637a08a99c2c8ad39aadb62e2303c82
Chien Le <chien.le@ptnglobalcorp.com>
2022-06-23 10:06:08 +0700

    Create base structure using 3 tier architecture

--- src/MeetingWebAPI/Program.cs
+++ src/MeetingWebAPI/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<Meeting_DAL.Data.AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MeetingDbContext") ?? throw new InvalidOperationException("Connection string 'EmployeeManagementApiContext' not found.")));
+
 // Add services to the container.
+builder.Services.AddDbContext<Meeting_DAL.Data.AppDbContext>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ src/Meeting_DAL/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Meeting_DAL.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Meeting_DAL.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<AppUser> AppUsers
+        {   get; set; }
+    }
+}
